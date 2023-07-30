ed472874272bae65247c833be592e1a898acd4e5
Jaeyoon <12190586@inha.edu>
2023-07-30 09:49:52 +0900

    7/30 코리아 타임 적용해버림

--- src/cam/docking.py
+++ src/cam/docking.py
@@ -9,16 +9,16 @@ import cv2 as cv
 import numpy as np
 from std_msgs.msg import UInt16
 from tricat231_pkg.msg import Cam
-import markDetection_0721
+import cam.markDetection as markDetection
 
 class Docking :
     def __init__(self):
         self.end = False
         self.webcam = cv.VideoCapture(0)
-        # if self.end:
-        #     self.webcam = cv.VideoCapture(0)
         self.detecting_color = rospy.get_param("detecting_color")
         self.detecting_shape = rospy.get_param("detecting_shape")
+        # self.detecting_color = 3
+        # self.detecting_shape = 3
 
         #sub
         self.count_sub = rospy.Subscriber("/count", UInt16, self.count_callback, queue_size=10)
@@ -33,28 +33,27 @@ class Docking :
         self.cam_control_angle = cam_control_angle
         self.cam_u_thruster = u_thruster
         self.cam_end = cam_end
-        self.cam_data_pub.publish(self.cam_control_angle,  self.cam_u_thruster, self.cam_end)
-    
+
+        cam_msg = Cam()
+        cam_msg.cam_control_angle.data = self.cam_control_angle
+        cam_msg.cam_u_thruster.data = self.cam_u_thruster
+        cam_msg.cam_end.data = self.cam_end
+        self.cam_data_pub.publish(cam_msg)
+
     def count_callback(self, msg):
         self.end = msg.data
         
-    def run(self):
-        # webcam = cv.VideoCapture(0)
-        # if not self.webcam.isOpened(): # 캠이 연결되지 않았을 경우 # true시 캠이 잘 연결되어있음
-        #     print("Could not open webcam")
-        #     exit()
-
-        # while self.webcam.isOpened(): 
+    def control(self):
         _, cam = self.webcam.read() # webcam으로 연결된 정보 읽어오기
-            # cv.imshow('webcam', cam) # webcam 창에 cam 보이기
-            # raw_img = cv.imread(webcam, cv.IMREAD_COLOR)
-            # cv.imshow("RAW_IMG", raw_img)
+        # cv.imshow('webcam', cam) # webcam 창에 cam 보이기
+        # raw_img = cv.imread(webcam, cv.IMREAD_COLOR)
+        # cv.imshow("RAW_IMG", raw_img)
 
         # 1. 영상 이미지 전처리
-        hsv_image = markDetection_0721.image_preprocessing(cam)
+        hsv_image = markDetection.image_preprocessing(cam)
 
         # 2. 탐지 색상 범위에 따라 마스크 형성
-        mask = markDetection_0721.color_filtering(self.detecting_color, hsv_image)
+        mask = markDetection.color_filtering(self.detecting_color, hsv_image)
 
         # 3. 형성된 마스크에서 외곽선 검출 ( datatype 변경 )
         contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) # 컨투어 검출
@@ -63,11 +62,11 @@ class Docking :
         #    contours = contours.astype(np.float)
             
         # contour_info, raw_image = markDetection.shape_and_label(self.detecting_shape, raw_image, contours)
-        contour_info, raw_image = markDetection_0721.shape_and_label(self.detecting_shape, cam, contours, min_area)
+        contour_info, raw_image = markDetection.shape_and_label(self.detecting_shape, cam, contours, min_area)
         cv.imshow("CONTROLLER", raw_image)
         # h,w,c = raw_image.shape # 원본 이미지에서 가로 길이 받아오기
         # move_with_largest(contour_info, w)
-        control_angle, thruster_value, size = markDetection_0721.move_with_largest(contour_info, raw_image.shape[1])
+        control_angle, thruster_value, size = markDetection.move_with_largest(contour_info, raw_image.shape[1])
 
         return control_angle, thruster_value, size 
 
@@ -88,23 +87,19 @@ def main():
     rospy.init_node('Docking')
     
     if not docking.webcam.isOpened(): # 캠이 연결되지 않았을 경우 # true시 캠이 잘 연결되어있음
-            print("Could not open webcam")
-            exit()
-            
-    while docking.webcam.isOpened():
-        cam_control_angle, u_thruster, size = docking.run()
-        if (cv.waitKey(1) & 0xFF == 27) or size == 100: # esc버튼 누르면 창 꺼짐
-            docking.finish = True
-            break
+        print("Could not open webcam")
+        exit()
             
     rate = rospy.Rate(10) # 10Hz
-    
-    try:
-        while not rospy.is_shutdown():
-            docking.publish_value(cam_control_angle, u_thruster, docking.cam_end)
-            rate.sleep()
-    except rospy.ROSInterruptException:
-        pass
+    # while docking.webcam.isOpened():
+    while not rospy.is_shutdown():
+        cam_control_angle, cam_u_thruster, size = docking.control()
+        docking.publish_value(cam_control_angle, cam_u_thruster, docking.cam_end)
+        if (cv.waitKey(1) & 0xFF == 27) or size == 100: # 카메라 창에서 esc버튼 꺼짐
+            docking.cam_end = True
+            docking.publish_value(cam_control_angle, cam_u_thruster, docking.cam_end)
+            break
+        rate.sleep()        
 
 if __name__=="__main__":
     main()
--- src/cam/markDetection.py
+++ src/cam/markDetection.py
@@ -4,96 +4,7 @@
 import cv2 as cv
 import numpy as np
 
-def color_set():
-    # 주황
-    [255,127,0]
-
-# 이미지 내 특정 색상 검출 함수
-def color_filtering(detecting_color, hsv_image):     
-    if detecting_color == 1: # Blue
-        lower_color = np.array([110, 50, 50])
-        upper_color = np.array([130, 255, 255])
-        mask = cv.inRange(hsv_image, lower_color, upper_color) # 색상 범위에 해당하는 마스크 생성
-    elif detecting_color == 2: # Green
-        lower_color = np.array([50, 50, 50])
-        upper_color = np.array([70, 255, 255])
-        mask = cv.inRange(hsv_image, lower_color, upper_color)
-    elif detecting_color == 3: # Red
-        lower_color = np.array([0, 50, 50])
-        upper_color = np.array([10, 255, 255])
-        lower_color2 = np.array([170, 50, 50])
-        upper_color2 = np.array([180, 255, 255])
-        mask = cv.inRange(hsv_image, lower_color, upper_color) + cv.inRange(hsv_image, lower_color2, upper_color2)
-    else: # J: 예외 처리로 넣었습니다.
-        pass
-
-    return mask
-
-# 원하는 도형의 윤곽선 면적과 중심점 찾기, 도형 labelling
-def shape_and_label(detecting_shape, raw_image, contours):    
-    contour_info = []
-    for contour in contours:
-        approx = cv.approxPolyDP(contour, cv.arcLength(contour, True) * 0.01, True)
-        
-        # cv2.approxPolyDP(curve, epsilon, closed, approxCurve=None) 
-        # -> approxCurve : 외곽선을 근사화(단순화)
-        #   • curve: 입력 곡선 좌표. numpy.ndarray. shape=(K, 1, 2)
-        #   • epsilon: 근사화 정밀도 조절. 입력 곡선과 근사화 곡선 간의 최대 거리. e.g) cv2.arcLength(curve) * 0.02
-        #   • closed: True를 전달하면 폐곡선으로 인식
-        #   • approxCurve: 근사화된 곡선 좌표. numpy.ndarray. shape=(K', 1, 2)
-
-        # cv2.arcLength(curve, closed) 
-        # -> retval: 외곽선 길이를 반환
-        #   • curve: 외곽선 좌표. numpy.ndarray. shape=(K, 1, 2)
-        #   • closed: True이면 폐곡선으로 간주
-        #   • retval: 외곽선 길이 
-
-        line_num = len(approx)
-        if detecting_shape == 0:  # 원 ### 수정이필요해 ~ㅠ  // J:???
-            _, radius = cv.minEnclosingCircle(approx)  # 원으로 근사
-            ratio = radius * radius * 3.14  # 해당 넓이와 정원 간의 넓이 비
-            if 0.5 < ratio < 2:  # 원에 가까울 때만 필터링
-                area, center = find_area_centroid(contour)
-                setLabel(raw_image, contour, 'CIRCLE')
-            else:
-                area, center = None, None
-        elif detecting_shape == 3 and line_num == 3:  # 삼각형
-            area, center = find_area_centroid(contour)
-            setLabel(raw_image, contour, 'TRIANGLE')
-        elif detecting_shape == 4 and line_num == 4:  # 사각형
-            area, center = find_area_centroid(contour)
-            setLabel(raw_image, contour, 'RECTANGLE')
-        elif detecting_shape == 12 and line_num == 12:  # 십자가
-            area, center = find_area_centroid(contour)
-            setLabel(raw_image, contour, 'CROSS')
-        else:
-            continue
-
-        if area is not None and center is not None:
-            contour_info.append((area, center))
-            cv.circle(raw_image, center, 5, (255, 0, 0), -1)
-
-    return contour_info, raw_image
-
-def find_area_centroid(contour):
-    # 윤곽선의 면적 계산
-    area = cv.contourArea(contour)
-    center = (0,0)
-    # 윤곽선의 중심점 계산
-    if area > 10000 : # 인식 면적 제한 두기
-        M = cv.moments(contour)
-        if M["m00"] != 0:
-            cx = int(M["m10"] / M["m00"])
-            cy = int(M["m01"] / M["m00"])
-            center = (cx, cy)
-            return area, center
-        else:
-            return None, None      
-    else :
-        # print('중심점계산오류')
-        return None, None
-
-# 영상 이미지 전처리 함수
+# 영상 이미지 전처리
 def image_preprocessing(cam): 
     raw_image = cam
     img0 = mean_brightness(raw_image) # 평균 밝기로 보정하는 함수
@@ -101,19 +12,126 @@ def image_preprocessing(cam):
     hsv_image = cv.cvtColor(img, cv.COLOR_BGR2HSV) # BGR 형식의 이미지를 HSV 형식으로 전환
     return hsv_image
 
-# 탐지 범위에 따른 마스크 형성 및 외곽선 검출하는 함수
-def show_the_shape_contour(hsv_image,detecting_color):   
+def mean_brightness(img):
+    fixed = 100  # 이 값 주변으로 평균 밝기 조절함
+    m = cv.mean(img)  # 평균 밝기
+    scalar = (-int(m[0]) + fixed, -int(m[1]) + fixed, -int(m[2]) + fixed, 0)
+    dst = cv.add(img, scalar)
+    return dst
+
+# 탐지 범위에 따른 마스크 형성 및 외곽선 검출  
+def show_the_shape_contour(hsv_image,detecting_color):
     mask = color_filtering(detecting_color, hsv_image)
     contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) # 컨투어 검출
     contours = np.array(contours)
 #    contours = contours.astype(np.float)
     return contours
 
+# 이미지 내 특정 색상 검출
+def color_filtering(detecting_color, hsv_image):
+    if detecting_color == 1: # Blue
+        lower_color = np.array([110, 50, 50]) # np.array([100, 100, 100])
+        upper_color = np.array([130, 255, 255]) # np.array([130, 255, 255])
+        mask = cv.inRange(hsv_image, lower_color, upper_color) # 색상 범위에 해당하는 마스크 생성
+    elif detecting_color == 2: # Green
+        lower_color = np.array([50, 50, 50]) # np.array([40, 100, 100])
+        upper_color = np.array([70, 255, 255]) # np.array([80, 255, 255])
+        mask = cv.inRange(hsv_image, lower_color, upper_color)
+    elif detecting_color == 3: # Red
+        lower_color = np.array([0, 100, 100]) 
+        upper_color = np.array([10, 255, 255]) 
+        mask = cv.inRange(hsv_image, lower_color, upper_color)
+    elif detecting_color == 4: # Orange
+        lower_color = np.array([10, 100, 100])
+        upper_color = np.array([25, 255, 255])
+        mask = cv.inRange(hsv_image, lower_color, upper_color)
+    elif detecting_color == 5: # Black
+        lower_color = np.array([0, 0, 0])
+        upper_color = np.array([255, 255, 30]) 
+        mask = cv.inRange(hsv_image, lower_color, upper_color)
+    else:
+        pass
+    return mask
+
 def show_the_shape_info(raw_image, detecting_shape,contours) :
     contour_info, raw_image = shape_and_label(detecting_shape, raw_image, contours)
 #    cv.imshow("CONTROLLER", raw_image)
-    servo_angle, thruster_value = move_with_largest(contour_info, raw_image.shape[1]) # return : servo, thruster
-    return raw_image, servo_angle, thruster_value
+    control_angle_angle, thruster_value = move_with_largest(contour_info, raw_image.shape[1]) # return : control_angle, thruster
+    return raw_image, control_angle_angle, thruster_value
+
+###################################
+# JJU_0721수정_3 : 함수 이름 find_centroid로 수정했음
+# JJU_ : docking.py에서 함수 이름 바꿔주시고, shape_and_label이랑 find_centroid return type 확인해서 수정해야 합니당
+###################################
+
+def find_centroid(contour):
+    center = (0,0)
+    # 윤곽선의 중심점 계산
+    M = cv.moments(contour)
+    if M["m00"] != 0:
+        cx = int(M["m10"] / M["m00"])
+        cy = int(M["m01"] / M["m00"])
+        center = (cx, cy)
+        return center
+    else:
+        return None
+ 
+###################################
+# JJU_0721수정_2 : 원 인식 부분 수정함 & area 변수 계산 추가함 & shape_and_label 함수 매개변수 min_area 추가함
+# JJU_ : 지난번에 수정 안된걸 드렸나봐요 (?). 원 인식을 위해서 원래 뒤에 find_area_centroid 함수에서 계산했던 'area' 연산을 이 함수로 뺐어용
+###################################
+
+def shape_and_label(detecting_shape, raw_image, contours, min_area):    # 원하는 도형의 윤곽선 면적과 중심점 찾기, 도형 labelling
+    contour_info = []
+    for contour in contours:
+        # 윤곽선의 면적 계산
+        area = cv.contourArea(contour)
+        if area < min_area: # 인식 면적 제한 두기
+            continue
+        approx = cv.approxPolyDP(contour, cv.arcLength(contour, True) * 0.01, True)
+
+        # cv2.approxPolyDP(curve, epsilon, closed, approxCurve=None) -> approxCurve : 외곽선을 근사화(단순화)
+        #   • curve: 입력 곡선 좌표. numpy.ndarray. shape=(K, 1, 2)
+        #   • epsilon: 근사화 정밀도 조절. 입력 곡선과 근사화 곡선 간의 최대 거리. e.g) cv2.arcLength(curve) * 0.02
+        #   • closed: True를 전달하면 폐곡선으로 인식
+        #   • approxCurve: 근사화된 곡선 좌표. numpy.ndarray. shape=(K', 1, 2)
+
+        # cv2.arcLength(curve, closed) -> retval: 외곽선 길이를 반환
+        #   • curve: 외곽선 좌표. numpy.ndarray. shape=(K, 1, 2)
+        #   • closed: True이면 폐곡선으로 간주
+        #   • retval: 외곽선 길이 
+
+        line_num = len(approx)                
+        if detecting_shape == 3 and line_num == 3:  # 삼각형
+            center = find_centroid(contour)
+            setLabel(raw_image, contour, 'TRIANGLE')
+        elif detecting_shape == 4 and line_num == 4:  # 사각형
+            center = find_centroid(contour)
+            setLabel(raw_image, contour, 'RECTANGLE')
+        elif detecting_shape == 12 and line_num == 12:  # 십자가
+            center = find_centroid(contour)
+            setLabel(raw_image, contour, 'CROSS')
+        else:
+            _, radius = cv.minEnclosingCircle(approx)  # 원으로 근사
+            ratio = radius * radius * 3.14 / (area + 0.000001)  # 해당 넓이와 정원 간의 넓이 비
+            if 0.5 < ratio < 2:  # 원에 가까울 때만 필터링
+                center = find_centroid(contour)
+                setLabel(raw_image, contour, 'CIRCLE')
+            else:
+                center = None
+
+        if area is not None and center is not None:
+            contour_info.append((area, center))
+            cv.circle(raw_image, center, 5, (255, 0, 0), -1)
+
+    return contour_info, raw_image
+
+def setLabel(img, pts, label):
+    (x,y,w,h) = cv.boundingRect(pts)
+    pt1 = (x,y)
+    pt2 = (x+w, y+h)
+    cv.rectangle(img, pt1, pt2, (0,255,0), 2)
+    cv.putText(img, label, (pt1[0], pt1[1]-3), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
 
 def move_with_largest(contour_info, raw_image_width):
     # 제일 큰 도형 선택
@@ -122,7 +140,7 @@ def move_with_largest(contour_info, raw_image_width):
 #    print("Contour Info After Filtering:", contour_info)  # Print contour_info after filtering
     Limage_limit = raw_image_width / 2 - 10
     Rimage_limit = raw_image_width / 2 + 10
-    servo = 93
+    control_angle = 93
     thruster = 1550 # thruster_mid
     size = 0
     if len(contour_info) > 0: # contour에 area, center가 입력되었을 때 ( 도형이 1개 이상 인식되었을 때 )
@@ -138,26 +156,28 @@ def move_with_largest(contour_info, raw_image_width):
         # and largest_width < raw_image_width / a: # center의 x좌표가 화면 절반보다 왼쪽에 있을 때 : 왼쪽으로 회전
         #    print(centroid_x, Limage_limit, largest_width, raw_image_width, "Move Left")
         #    print(contour_info)
+            # control_angle
             print("Left")
-            servo = 81
+            control_angle = 81
 
         elif centroid_x > Rimage_limit :
         # and largest_width < raw_image_width / a: # center의 x좌표가 화면 절반보다 오른쪽에 있을 때 : 오른쪽으로 회전
         #    print(centroid_x, Limage_limit, largest_width, raw_image_width, "Move Right")
         #    print(contour_info)
             print("Right")
-            servo = 105
+            control_angle = 105
 
-        elif Limage_limit < centroid_x < Rimage_limit and largest_width < raw_image_width / a :
-            print("Move Front")
-            size = 10
-            servo = 93
-            thruster = 1550 # thruster_max
-        elif Limage_limit < centroid_x < Rimage_limit and largest_width > raw_image_width / a :
-            print("STOP")
-            size = 100
-            servo = 93
-            thruster = 1500 # thruster_min
+        elif Limage_limit < centroid_x < Rimage_limit :
+            if largest_width < raw_image_width / a :
+                print("Move Front")
+                size = 10
+                control_angle = 93
+                thruster = 1550 # thruster_max
+            elif largest_width > raw_image_width / a :
+                print("STOP")
+                size = 100
+                control_angle = 93
+                thruster = 1500 # thruster_min
         ## 예외case
         # elif centroid_x < Limage_limit and largest_width > raw_image_width / a : 
         #     print("case1")
@@ -165,21 +185,8 @@ def move_with_largest(contour_info, raw_image_width):
         #     print("case2") 
     else:
         print("No contour found")
-    print("servo : ", servo, "thruster : ", thruster)
-    return servo, thruster, size
+    print("control_angle : ", control_angle, "thruster : ", thruster)
+    return control_angle, thruster, size
 #    print(contour_info)  # Print contour_info for debugging
 
-# 트렉바로 조절 하는 방식으로 값을 먼저 찾아야 할 것 같음(대회 시작 전에)
-def mean_brightness(img):
-    fixed = 100  # 이 값 주변으로 평균 밝기 조절함
-    m = cv.mean(img)  # 평균 밝기
-    scalar = (-int(m[0]) + fixed, -int(m[1]) + fixed, -int(m[2]) + fixed, 0)
-    dst = cv.add(img, scalar)
-    return dst
 
-def setLabel(img, pts, label):
-    (x,y,w,h) = cv.boundingRect(pts)
-    pt1 = (x,y)
-    pt2 = (x+w, y+h)
-    cv.rectangle(img, pt1, pt2, (0,255,0), 2)
-    cv.putText(img, label, (pt1[0], pt1[1]-3), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
