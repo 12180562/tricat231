8df4f250030ef4252b1bb19b270b1ce38f10e4bb
Jaeyoon <12190586@inha.edu>
2023-08-01 17:16:52 +0900

    8/1 Arduino

--- Arduino/211/211.ino
+++ Arduino/211/211.ino
@@ -29,7 +29,7 @@ Servo thruster1; //스러스터1
 Servo thruster2; //스러스터2
 Servo servo1;
 Servo servo2;    //서보모터
-Adafruit_NeoPixel neopixel = Adafruit_NeoPixel(led_num, led, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel neopixel = Adafruit_NeoPixel(led_num, led, NEO_GRB + NEO_KHZ800);
 
 void servo_cb( const std_msgs::UInt16& cmd_msg){
   servo1.write(cmd_msg.data-2);
@@ -43,6 +43,11 @@ void thruster_cb( const std_msgs::UInt16& cmd_msg){
 
 ros::Subscriber<std_msgs::UInt16> sub1("servo", servo_cb);
 ros::Subscriber<std_msgs::UInt16> sub3("thruster", thruster_cb);
+
+//uint32_t red = neopixel.Color(255, 0, 0, 0);
+//uint32_t green = neopixel.Color(0, 255, 0, 0);
+//uint32_t yellow = neopixel.Color(255, 255, 0, 0);
+//uint32_t white = neopixel.Color(0, 0, 0, 255);
 
 void setup()
 { 
@@ -63,51 +68,46 @@ void setup()
   servo2.attach(9); //서보모터 9번핀
   servo1.writeMicroseconds(95); // initial state is Neutral
   servo2.writeMicroseconds(95); 
-
-  neopixel.setBrightness(255); // 0~255 사이의 값으로 최대 밝기 조절 (255가 최대)
+  
   neopixel.begin();
-
-  uint32_t red = neopixel.Color(255, 0, 0, 0);
-  uint32_t green = neopixel.Color(0, 255, 0, 0);
-  uint32_t yellow = neopixel.Color(255, 255, 0, 0);
+  neopixel.show();
+  neopixel.setBrightness(255); // 0~255 사이의 값으로 최대 밝기 조절 (255가 최대)
+  neopixel.clear();
   //delay(1000);
   //Serial.begin(9600);
 }
 
 void loop()
 {
-    neopixel.clear();
+  neopixel.clear();
+  
     //unsigned long currentMillis = millis();
     ch5 = pulseIn(ip5,HIGH);
-  {
-    if(ch5 < 1450)
-      {
-        /*if (currentMillis - previousMillis >= 1000) {
-          previousMillis = currentMillis;
-          if (ledState == LOW) {
-            ledState = HIGH;
-          } else {
-            ledState = LOW;
-          }
-          digitalWrite(led, ledState);
-        } */
-        for(int i = 0; i < led_num; ++i){
-          neopixel.setPixelColor(i, yellow);
-          neopixel.show();
-          //delay(50); 
+    ch6 = pulseIn(ip6,HIGH);
+
+    if(ch6 > 1450){
+       for(int i = 0; i < led_num; i++){     
+        neopixel.setPixelColor(i,255,0,0);
+       }
+        neopixel.show();
+    }
+    
+    else{
+    if(ch5 < 1450){   
+        for(int i = 0; i < led_num; i++){       
+        neopixel.setPixelColor(i,255,255,0);
         }
+        neopixel.show();
+        
         autonomous();
       }
-    else if(ch5 > 1450)
-      {
-        rc();
-
-        for(int i = 0; i < led_num; ++i){
-          neopixel.setPixelColor(i, green);
-          neopixel.show();
-          //delay(50); 
+    else if(ch5 > 1450){
+        for(int j = 0; j < led_num; j++){       
+        neopixel.setPixelColor(j,0,255,0);
         }
-        //digitalWrite(led, HIGH);
+        neopixel.show();
+        
+        rc();
       }
-  }
+    }
 }
